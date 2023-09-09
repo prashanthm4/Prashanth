745f50fcc3b3c1df92713a7ad83de89f1d5a4e9e
prashanthm <142563153+prashanthm4@users.noreply.github.com>
2023-09-09 21:31:23 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -6,17 +6,7 @@
 #include <stdio.h>
  
 
-# feature 202 changes done by prashi on reg module
-// declare variables
-      int num1, num2, sum;
-
-      // take two numbers
-      num1 = 10;
-      num2 = 20;
-
-      // calculate sum value
-      sum = num1 + num2;
-
+# fea
   
 
 
