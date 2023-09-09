fae2e74a30f4f2453070fd2acfe44704059fddab
prashanthm <142563153+prashanthm4@users.noreply.github.com>
2023-09-09 18:20:44 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -9,14 +9,7 @@ void main()
 {
     int num;
  
-    printf("Enter a number: \n");
-    scanf("%d", &num);
-    if (num > 0)
-        printf("%d is a positive number \n", num);
-    else if (num < 0)
-        printf("%d is a negative number \n", num);
-    else
-        printf("0 is neither positive nor negative");
+  
  
 # feature 202 changes done by prashi on reg module
 // declare variables
